89ec1fd7a4d715c31c753cfdec75b7e8b93e6ab3
Sergio Peralta <serpel.js@gmail.com>
2017-07-21 17:02:08 -0600

    sales order with auth approver

--- OpenShopVHBackend/OpenShopVHBackend/BussinessLogic/SAP/SalesOrder.cs
+++ OpenShopVHBackend/OpenShopVHBackend/BussinessLogic/SAP/SalesOrder.cs
@@ -2,11 +2,15 @@
 using SAPbobsCOM;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
 
 namespace OpenShopVHBackend.BussinessLogic
 {
     class SalesOrder
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         private ServerConnection _connection;
         public ServerConnection ServerConnection {
             get {
@@ -41,6 +45,123 @@ namespace OpenShopVHBackend.BussinessLogic
         {
             set { this.lastMessage = value; }
             get { return this.lastMessage; }
+        }
+
+        public String AddSalesOrder(int orderId, int userId)
+        {
+            String lastMessage = "";
+            String key = "";
+            OrderStatus status = OrderStatus.CreadoEnAplicacion;
+
+            var user = db.DeviceUser.Where(w => w.DeviceUserId == userId).ToList().FirstOrDefault();
+            String connection = user.Shop == null ? "" : user.Shop.ConnectionString;
+
+            using (var db = new ApplicationDbContext(connection))
+            {
+                var order = db.Orders
+                    .Include(i => i.Client).Include(i => i.DeviceUser)
+                    .Where(w => w.OrderId == orderId)
+                    .ToList()
+                    .FirstOrDefault();
+
+                try
+                {
+                    if (order != null)
+                    {
+                        if (String.IsNullOrEmpty(order.RemoteId))
+                        {
+                            if (_connection.Connect() == 0)
+                            {
+                                company = _connection.GetCompany();
+                                salesOrder = company.GetBusinessObject(BoObjectTypes.oOrders);
+                                //salesOrder.DocObjectCode = BoObjectTypes.oOrders;
+                                salesOrder.CardCode = order.Client.CardCode;
+                                salesOrder.Comments = order.Comment;
+                                salesOrder.SalesPersonCode = order.DeviceUser.SalesPersonId;
+                                salesOrder.DocDueDate = order.DeliveryDate != null ? DateTime.Parse(order.DeliveryDate) : DateTime.Now.AddDays(2);
+
+                                if (salesOrder.UserFields.Fields.Count > 0)
+                                {
+                                    salesOrder.UserFields.Fields.Item("U_FacFecha").Value = DateTime.Now.ToShortDateString();
+                                    salesOrder.UserFields.Fields.Item("U_FacNit").Value = order.Client.RTN;
+                                    salesOrder.UserFields.Fields.Item("U_FacNom").Value = order.Client.Name;
+                                }
+
+                                foreach (var item in order.OrderItems)
+                                {
+                                    salesOrder.Lines.ItemCode = item.SKU;
+                                    salesOrder.Lines.Quantity = item.Quantity;
+                                    salesOrder.Lines.TaxCode = item.TaxCode;
+                                    salesOrder.Lines.DiscountPercent = item.DiscountPercent;
+                                    salesOrder.Lines.WarehouseCode = item.WarehouseCode;
+                                    salesOrder.Lines.Add();
+                                }
+
+                                //Autorization
+                                salesOrder.GetApprovalTemplates();
+                                if(salesOrder.Document_ApprovalRequests.Count > 0 && salesOrder.Document_ApprovalRequests.ApprovalTemplatesID > 0)
+                                {
+                                    salesOrder.Document_ApprovalRequests.SetCurrentLine(0);
+                                    salesOrder.Document_ApprovalRequests.Remarks = "Add Doc from DI API";
+                                }
+
+                                // add Sales Order to draft
+                                if (salesOrder.Add() == 0)
+                                {
+                                    key = company.GetNewObjectKey();
+                                    lastMessage = String.Format("Successfully, DocEntry: {0}", key);
+                                    MyLogger.GetInstance.Info(lastMessage);
+                                    status = OrderStatus.PreliminarEnSAP;
+                                }
+                                else
+                                {
+                                    lastMessage = "Error Code: "
+                                            + company.GetLastErrorCode().ToString()
+                                            + " - "
+                                            + company.GetLastErrorDescription();
+
+                                    status = OrderStatus.ErrorAlCrearEnSAP;
+                                }
+                                //recomended from http://www.appseconnect.com/di-api-memory-leak-in-sap-business-one-9-0/
+                                System.Runtime.InteropServices.Marshal.ReleaseComObject(salesOrder);
+                                salesOrder = null;
+                                //company.Disconnect();
+                            }
+                            else
+                            {
+                                lastMessage = "AddSalesOrder - Connection error: "
+                                        + _connection.GetErrorMessage().ToString();
+
+                                status = OrderStatus.ErrorAlCrearEnSAP;
+                            }
+
+                            if (order != null)
+                            {
+                                order.RemoteId = key;
+                                order.LastErrorMessage = lastMessage;
+                                order.Status = status;
+                                db.Entry(order).State = System.Data.Entity.EntityState.Modified;
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    lastMessage += "AddSalesOrder - error" + e.Message;
+                    MyLogger.GetInstance.Error(lastMessage, e);
+
+                    if (order != null)
+                    {
+                        order.LastErrorMessage = lastMessage;
+                        order.Status = OrderStatus.ErrorAlCrearEnSAP;
+                        db.Entry(order).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+
+                return key;
+            }
         }
 
         public String AddSalesOrder(Order order, List<OrderItem> items)
